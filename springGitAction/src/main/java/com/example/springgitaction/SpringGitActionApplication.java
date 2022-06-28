e5ef1070bda6b6522fa10d919d92c692fbb3f53f
Yoon321 <tkdwls734@gmail.com>
2022-06-28 20:25:06 +0900

    initial setting

---
+++ springGitAction/src/main/java/com/example/springgitaction/SpringGitActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGitActionApplication.class, args);
+    }
+
+}
---
+++ springGitAction/src/test/java/com/example/springgitaction/SpringGitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
