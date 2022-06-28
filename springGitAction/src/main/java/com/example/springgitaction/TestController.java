e2a8280cd3f120f97caa531896fb491d5a3414d0
Yoon321 <tkdwls734@gmail.com>
2022-06-28 22:31:24 +0900

    test

--- springGitAction/src/main/java/com/example/springgitaction/TestController.java
+++ springGitAction/src/main/java/com/example/springgitaction/TestController.java
@@ -1,13 +1,16 @@
 package com.example.springgitaction;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
     @GetMapping
     public String test(){
-        return "test";
+        return "My test";
     }
 }
